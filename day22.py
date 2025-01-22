be212fe9c13a069d8e87428e7e7cff8077503182
Vaka Jahnavi <139772025+jahnavi0111@users.noreply.github.com>
2025-01-22 14:59:32 +0530

    Create day22.py

---
+++ day22.py
@@ -0,0 +1,24 @@
+class Solution {
+    // Function to find two elements in array
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        int n=arr.length;
+        ArrayList<Integer> result=new ArrayList<>();
+        int[] freq=new int[n+1];
+        for(int i=0; i<n; i++){
+            freq[arr[i]]++;
+        }
+        int rep=-1;
+        int miss=-1;
+        for(int i=1; i<=n; i++){
+            if(freq[i]==2){
+                rep=i;
+            }
+            else if(freq[i]==0){
+                miss=i;
+            }
+        }
+        result.add(rep);
+        result.add(miss);
+        return result;
+        // code here
+    }
